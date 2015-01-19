f448a41f13810ff4b1a475d8b9f75ea1a8e87d6a
Deepak Shinde <deepak@quadnode.com>
2015-01-19 20:47:30 +0530

    added options for image, css, font and javascript

--- lib/kristin.rb
+++ lib/kristin.rb
@@ -27,6 +27,12 @@ module Kristin
 
     def process_options
       opts = []
+      
+      opts.push("--embed-image 0") if @options[:embed_image] == false
+      opts.push("--embed-css 0") if @options[:embed_css] == false
+      opts.push("--embed-font 0") if @options[:embed_font] == false
+      opts.push("--embed-javascript 0") if @options[:embed_javascript] == false
+
       opts.push("--process-outline 0") if @options[:process_outline] == false
       opts.push("--first-page #{@options[:first_page]}") if @options[:first_page]
       opts.push("--last-page #{@options[:last_page]}") if @options[:last_page]
